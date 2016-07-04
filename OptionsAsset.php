0e605698e46e2a29ceb8d43edec04ab22f2b2cd6
porcelanosa <public_mce@rambler.ru>
2016-07-04 13:54:01 +0300

    fix2 composer.json

--- OptionsAsset.php
+++ OptionsAsset.php
@@ -1,22 +1,21 @@
 <?php
 	
 	namespace porcelanosa\yii2options;
-
-use Yii;
-use yii\web\AssetBundle;
-
-class OptionsAsset extends AssetBundle
-{
-    public $sourcePath = '@app/behaviors/options/assets';
-    public $js = [
-        'options.js',
-    ];
-    public $css = [
-        'options.css'
-    ];
-    public $depends = [
-        'yii\web\JqueryAsset',
-        'yii\jui\JuiAsset'
-    ];
-
-}
+	
+	use Yii;
+	use yii\web\AssetBundle;
+	
+	class OptionsAsset extends AssetBundle {
+		public $sourcePath = '@app/behaviors/options/assets';
+		public $js = [
+			'options.js',
+		];
+		public $css = [
+			'options.css'
+		];
+		public $depends = [
+			'yii\web\JqueryAsset',
+			'yii\jui\JuiAsset'
+		];
+		
+	}
